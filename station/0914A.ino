fbda3bb35c34c403663f1f904ca078babfc564d2
93i7xo2 <gr7p4zxh@gmail.com>
2015-09-17 21:41:24 +0800

    software is done. need to be test.

--- station/0914A.ino
+++ station/0914A.ino
@@ -11,9 +11,14 @@ const int receive_pin = 3;
 const int transmit_en_pin = 4;
 int ad_convertor_pin; //A0~A2
 VirtualSteper m1(5,6,7,8),m2(9,10,11,12);
+const int limit_pin=A3;
+int previous_dir;
 
 //function prototype
 void func1();
+int* c_max(int*,int*);
+int* c_min(int*,int*);
+int* c_find(int*,int*,int);
 
 
 void setup()
@@ -70,13 +75,31 @@ void loop()
             ad_convertor_pin=A2;
             func1();
         }
-        if(!strcmp(reinterpret_cast<char *>(buf),"LEFT"));
-        if(!strcmp(reinterpret_cast<char *>(buf),"RIGHT"));
+        if(!strcmp(reinterpret_cast<char *>(buf),"LEFT"))
+        {
+            m2.rotate(-0.1);
+            previous_dir=-1;
+        }
+        if(!strcmp(reinterpret_cast<char *>(buf),"RIGHT"))
+        {
+            m2.rotate(0.1);
+            previous_dir=1;
+        }
         if(!strcmp(reinterpret_cast<char *>(buf),"RESTART"))
         {
             soft_restart();
         }
-
+        if(!strcmp(reinterpret_cast<char *>(buf),""))
+        {
+            if(previous_dir=-1)
+            {
+                m2.rotate(-0.1);
+            }
+            else
+            {
+                m2.rotate(0.1);
+            }
+        }
     }
 
 }
@@ -84,16 +107,71 @@ void loop()
 void func1()
 {
     /*rotate to one side*/
-    m1.rotate(1);
+    if(analogRead(limit_pin)<100)
+    {
+        m1.rotate(0.1);
+    }
     /*rotate by 180 degree ,and recode ad value every 1 degree*/
     int ad_value_array[181];
     int i;
-    for(i=0;i<180;i++)
+    for(i=0; i<180; i++)
     {
         ad_value_array[i]=analogRead(ad_convertor_pin);
+        Serial.print(i);
+        Serial.print("=");
         Serial.println(ad_value_array[i]);
         m1.rotate(1);
     }
     ad_value_array[i]=analogRead(ad_convertor_pin);
+    Serial.print(i);
+    Serial.print("=");
     Serial.println(ad_value_array[i]);
+    /*find max value by used binary tree method*/
+    int* ad_max_value=c_max(ad_value_array,ad_value_array+181);
+    int* ad_min_value=c_min(ad_value_array,ad_value_array+181);
+    int* ad_max_index=c_find(ad_value_array,ad_value_array+181,*ad_max_value);
+    int* ad_min_index=c_find(ad_value_array,ad_value_array+181,*ad_min_value);
+    Serial.print("Max:");
+    Serial.println(*ad_max_value);
+    Serial.println(ad_max_index-ad_value_array);
+    Serial.print("Min:");
+    Serial.println(*ad_min_value);
+    Serial.println(ad_min_index-ad_value_array);
+    /*rotate to ad_max_position*/
+    int temp;
+    temp=-(180-(ad_max_index-ad_value_array));
+    Serial.print("Rotate angle:");
+    Serial.println(temp);
+    m1.rotate(temp);
+    Serial.println("Finish");
+}
+
+int* c_max(int * first,int * last)
+{
+    if (first==last) return last;
+    int* largest = first;
+
+    while (++first!=last)
+        if (*largest<*first)
+            largest=first;
+    return largest;
+}
+int* c_min(int * first,int * last)
+{
+    if (first==last) return last;
+    int* smallest = first;
+
+    while (++first!=last)
+        if (*first<*smallest)    // or: if (comp(*first,*smallest)) for version (2)
+            smallest=first;
+    return smallest;
+}
+int* c_find(int * first,int * last,int val)
+{
+    while (first!=last)
+    {
+        if (*first==val) return first;
+        ++first;
+    }
+    return last;
 }
